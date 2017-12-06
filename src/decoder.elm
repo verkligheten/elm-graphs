6476917f93d85d76138c3cd75e24b7b1a60d77e4
Igor Gulidov <f1xxx.ua@gmail.com>
2017-12-06 15:34:25 +0200

    models

--- src/decoder.elm
+++ src/decoder.elm
@@ -10,7 +10,7 @@ getRippleExchanges : Cmd Msg
 getRippleExchanges =
     let
         url =
-            "https://data.ripple.com/v2/exchanges/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
+            "https://data.ripple.com/v2/exchanges/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B?limit=1000&interval=30minute&start=2017-12-04T15:30:00&end=2017-12-05T15:30:00"
     in
     Http.send SetRates (Http.get url decodeExchangeData)
 
@@ -18,28 +18,26 @@ getRippleExchanges =
 decodeExchangeData : Json.Decode.Decoder Model
 decodeExchangeData =
     Pipeline.decode Model
-        |> Pipeline.required "result" Json.Decode.string
-        |> Pipeline.required "count" Json.Decode.int
-        |> Pipeline.required "marker" Json.Decode.string
-        |> Pipeline.required "exchanges" (Json.Decode.list decodeExchangeItem)
+      |> Pipeline.required "result" Json.Decode.string
+      |> Pipeline.required "count" Json.Decode.int
+      |> Pipeline.required "exchanges" (Json.Decode.list decodeExchangeItem)
 
 
 decodeExchangeItem : Json.Decode.Decoder ExchangeItem
 decodeExchangeItem =
     Pipeline.decode ExchangeItem
-        |> Pipeline.required "base_amount" Json.Decode.string
-        |> Pipeline.required "counter_amount" Json.Decode.string
-        |> Pipeline.required "node_index" Json.Decode.int
-        |> Pipeline.required "rate" Json.Decode.string
-        |> Pipeline.required "buyer" Json.Decode.string
-        |> Pipeline.required "executed_time" Json.Decode.string
-        |> Pipeline.required "ledger_index" Json.Decode.int
-        |> Pipeline.required "offer_sequence" Json.Decode.int
-        |> Pipeline.required "seller" Json.Decode.string
-        |> Pipeline.required "taker" Json.Decode.string
-        |> Pipeline.required "tx_hash" Json.Decode.string
-        |> Pipeline.required "tx_type" Json.Decode.string
-        |> Pipeline.required "tx_index" Json.Decode.int
-        |> Pipeline.required "base_currency" Json.Decode.string
-        |> Pipeline.required "counter_currency" Json.Decode.string
-        |> Pipeline.required "counter_issuer" Json.Decode.string
+      |> Pipeline.required "base_volume" (Json.Decode.string)
+      |> Pipeline.required "buy_volume" (Json.Decode.float)
+      |> Pipeline.required "close" (Json.Decode.string)
+      |> Pipeline.required "close_time" (Json.Decode.string)
+      |> Pipeline.required "count" (Json.Decode.int)
+      |> Pipeline.required "counter_volume" (Json.Decode.string)
+      |> Pipeline.required "high" (Json.Decode.string)
+      |> Pipeline.required "low" (Json.Decode.string)
+      |> Pipeline.required "open" (Json.Decode.string)
+      |> Pipeline.required "open_time" (Json.Decode.string)
+      |> Pipeline.required "start" (Json.Decode.string)
+      |> Pipeline.required "vwap" (Json.Decode.string)
+      |> Pipeline.required "base_currency" (Json.Decode.string)
+      |> Pipeline.required "counter_currency" (Json.Decode.string)
+      |> Pipeline.required "counter_issuer" (Json.Decode.string)
